4922eb82c6b7b8b9210e6e9f3e5213ab611950ae
Prafull Prince <159343572+prafullprince@users.noreply.github.com>
2024-08-07 08:53:46 +0000

    maximal square problem solved

--- main.cpp
+++ main.cpp
@@ -737,3 +737,42 @@ int main(){
     }
 */
 
+// Q28 => maximal sqaure
+/*
+    int solve(vector<vector<char>>& matrix,int i,int j,int row,int col,int &maxi){
+        // base case
+        if(i>=row || j>=col){
+            return 0;
+        }
+
+        // 1 case
+        // all possible direction
+        int right = solve(matrix,i,j+1,row,col,maxi);
+        int diagonal = solve(matrix,i+1,j+1,row,col,maxi);
+        int down = solve(matrix,i,j+1,row,col,maxi);
+
+        // 
+        if(matrix[i][j] == '1'){
+            int ans = 1 + min(right,min(diagonal,down));
+            maxi = max(maxi,ans);
+            return ans;
+        }
+        else{
+            // on 0 matrix
+            return 0;
+        }
+        // return maxi;
+    }
+
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int i=0;
+        int j=0;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int maxi = 0;
+        solve(matrix,i,j,row,col,maxi);
+        return maxi*maxi;
+    }
+
+*/
+
